ea5d0c0b672fc03a533de317070cf014d9d794ab
leishengqiang <1740540069@qq.com>
2024-11-23 21:09:40 +0800

    第一次提交

---
+++ src/main/java/com/example/configuration/PageConfiguration.java
@@ -0,0 +1,21 @@
+package com.example.configuration;
+
+import com.baomidou.mybatisplus.extension.plugins.inner.OptimisticLockerInnerInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class PageConfiguration {
+    //分页插件
+    @Bean
+    public PaginationInnerInterceptor paginationInterceptor(){
+        return new PaginationInnerInterceptor();
+    }
+
+    //@version插件
+    @Bean
+    public OptimisticLockerInnerInterceptor optimisticLockerInnerInterceptor(){
+        return new OptimisticLockerInnerInterceptor();
+    }
+}
